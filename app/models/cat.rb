e62299445ceaa13c0b66258f23ca217eadf73c4b
raquel king <kingraq1007@gmail.com>
2023-09-06 15:31:39 -0500

    Validations, API completed

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,4 @@
 class Cat < ApplicationRecord
+    validates :name, :age, :enjoys, presence: true
+    validates :enjoys, presence: true, length: { minimum: 10 }
 end
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it "should validate name" do
+    cat = Cat.create(age: 4, enjoys: 'Hiding under the bed', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OLZBC9oeEXzls8Nsm8N7xge0jlfqlpHRoQ&usqp=CAU')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it "should validate age" do
+  cat = Cat.create(name: 'Princess Peach', enjoys: 'Hiding under the bed', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OLZBC9oeEXzls8Nsm8N7xge0jlfqlpHRoQ&usqp=CAU')
+  expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it "should validate enjoys" do
+    cat = Cat.create(name: 'Princess Peach', age: 3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OLZBC9oeEXzls8Nsm8N7xge0jlfqlpHRoQ&usqp=CAU')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate enjoys length" do
+  cat = Cat.create(name: 'Princess Peach', age: 4, enjoys: 'Hiding', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OLZBC9oeEXzls8Nsm8N7xge0jlfqlpHRoQ&usqp=CAU')
+  expect(cat.errors[:enjoys]).to_not be_empty
+  end
 end

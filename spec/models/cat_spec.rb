b9ef289c1b33483ea0f80517beebc65a0bfa9ed7
raquel king <kingraq1007@gmail.com>
2023-09-06 17:46:57 -0500

    image validation added

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -20,4 +20,9 @@ RSpec.describe Cat, type: :model do
   cat = Cat.create(name: 'Princess Peach', age: 4, enjoys: 'Hiding', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OLZBC9oeEXzls8Nsm8N7xge0jlfqlpHRoQ&usqp=CAU')
   expect(cat.errors[:enjoys]).to_not be_empty
   end
+
+  it "should validate image" do
+    cat = Cat.create(name: 'Princess Peach', age: 4, enjoys: 'Hiding under the bed')
+    expect(cat.errors[:image]).to_not be_empty
+  end
 end
